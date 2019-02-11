af404703baa0c15ace241e80a09851ab5e9fb476
Alan Choi <alanchoi08@gmail.com>
2019-02-10 22:52:50 -0800

    comment out skip before

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module Api
   class Api::SessionsController < ApplicationController
-    skip_before_action :ensure_logged_in, only: %i[new create failure validate_token]
+    # skip_before_action :ensure_logged_in, only: %i[new create failure validate_token]
     skip_before_action :verify_authenticity_token, only: %i[create failure]
     skip_after_action :verify_policy_scoped
     skip_after_action :verify_authorized
